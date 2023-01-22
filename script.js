8926e4aba33bedf024f8af7b321501794f781d52
Anthony Cob <73517248+AntoCobRamos2003@users.noreply.github.com>
2023-01-22 00:50:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // background sound
 const unmute = document.querySelector(".unmute");
 const mute = document.querySelector(".mute");
-let track = new Audio("piano.mp3");
+let track = new Audio("trav.mp3");
 track.volume = 0.1;
 track.loop = true;
-track.currentTime = 4.0;
+track.currentTime = 20.0;
 unmute.addEventListener("click", () => {
   if (mute.classList.contains("mute")) {
     mute.classList.add("active");
@@ -19,3 +19,20 @@ mute.addEventListener("click", () => {
     track.play();
   }
 });
+//animation
+AOS.init();
+//add animation to service cards
+const cards = document.querySelectorAll('.proCard');
+
+window.addEventListener('load',()=>{
+    for(let i=0;i<=cards.length;i++){
+        cards[i].setAttribute('data-aos-delay','300')
+    }
+})
+//add another animation when on mobile screen
+const ww = document.body.clientWidth;
+    if (ww < 600) {
+      for(let i=0;i<=cards.length;i++){
+        cards[i].setAttribute('data-aos','fade-up')
+    }
+  } 
